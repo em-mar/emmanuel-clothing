39139aed4fc8b7bb336ceda540d4d4f0c7864695
em-mar <marem301030@gmail.com>
2023-05-22 14:18:01 -0700

    added sign in component and brought firebase libraries.

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import {signInWithGooglePopup, createUserDocumentFromAuth  } from "../../utils/f
 function SignIn() {
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
-        createUserDocumentFromAuth(user)
+        const userDocRef = await createUserDocumentFromAuth(user)
         
     }
     return(
